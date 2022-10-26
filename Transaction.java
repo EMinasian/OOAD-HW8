40a98511465b9511eeb959d0d8d49edc7456f8c3
Anna-Gevorgyan <56083996+Anna-Gevorgyan@users.noreply.github.com>
2022-10-26 08:50:24 +0400

    Dont know if it is right

--- Transaction.java
+++ Transaction.java
@@ -1,59 +1,48 @@
-//package Design Class Diagram;
 
+//package Design Class Diagram;
+import java.util.Random;
+import java.util.ArrayList;
 import java.util.*;
 
 /**
  * 
  */
 public class Transaction {
-
-    /**
-     * Default constructor
-     */
-    public Transaction() {
-    }
-
-    /**
-     * 
-     */
-    private void actorID;
-
-    /**
-     * 
-     */
-    private void productID;
-
-    /**
-     * 
-     */
-    private void quantity;
-
-    /**
-     * 
-     */
-    private void time;
-
-    /**
-     * 
-     */
-    private void totalPrice;
-
-    /**
-     * 
-     */
-    private TransactionID transactionID;
-
-    /**
-     * 
-     */
-    private void type;
-
+	
+	private int actorID;
+    private int productID;
+    private int quantity = 0;
+    private String time;
+    private double totalPrice;
+    private int transactionID;
+    private String type;
+  
     /**
      * @param time 
      * @param o
      */
-    public void create(void time, void o) {
-        // TODO implement here
+    /**
+     * Default constructor
+     */
+    public Transaction(Offer o, String time1) {
+    	actorID = o.getActor().getID();
+    	productID = o.getProduct().getID();
+    	quantity = o.getQuantity();
+    	time = time1;
+    	totalPrice = o.getTotal();
+    	transactionID = generateID();
+    	type = o.getType();
     }
+    
+    private int generateID() {
+    	Random rand = new Random(); 
+        int upperbound = 1234567890;
+        int int_random = rand.nextInt(upperbound); 
+        // Should have some list of Ids to not have repetitions 
+        return int_random;
+    }
+    /// Should we add getters and setters here ? 
+
+   
 
 }
